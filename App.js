68825fa8b9646a97ab79900070d18ff36405d47a
Akila <akilachandran25@gmail.com>
2025-11-04 17:48:03 +0530

    Readme file

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading = React.createElement("h1", {}, "Hello World form React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const parent = React.createElement("div", {id: "parent"},
+    React.createElement("div",{id: "child1"},
+        React.createElement("h1",{}, "Hi, I'm the child one"),
+        React.createElement("h2",{}, "Hi, I'm the child two"),
+    ),
+    React.createElement("div",{id: "child2"},
+        React.createElement("p",{}, "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, distinctio!")
+    )
+);
+const headings = ReactDOM.createRoot(document.getElementById("headings"));
+headings.render(parent);
